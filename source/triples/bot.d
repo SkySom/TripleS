c30cc26230a343bdafa07fa1848cc8888eb1aa4a
SkySom <skylar.som@gmail.com>
2014-07-08 01:38:59 -0400

    Got some code in for starting the bot.
    Still not connecting to IRC.

--- source/triples/bot.d
+++ source/triples/bot.d
@@ -1,9 +1,21 @@
 module triples.bot;
 
 import std.stdio;
+import std.socket : Socket;
+
+import irc.client;
+import irc.eventloop;
+
 
 class TripleS {
   this() {
-    writeln("starting bot");
+    writeln("Starting TripleS");
+    auto connection = new IrcClient();
+    auto ircAddress = new InternetAddress("irc.freenode.net", 6667);
+    connection.connect(ircAddress);
+    auto eventloop = new IrcEventLoop();
+    eventloop.add(connection);
   }
+
+
 }
