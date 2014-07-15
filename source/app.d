4fe05b31bab06924f5768e9ee5d6f17336ec9df8
SkySom <skylar.som@gmail.com>
2014-07-15 16:08:04 -0400

    Added some code to test shutting down dirk
    Using some from vibe.d for now.

--- source/app.d
+++ source/app.d
@@ -1,4 +1,9 @@
 import std.stdio;
+import core.atomic;
+import core.sync.condition;
+import core.sync.mutex;
+import core.stdc.stdlib;
+import core.thread;
 
 import vibe.d;
 
@@ -11,6 +16,20 @@ shared static this()
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
 	listenHTTP(settings, routes);
-	
-	auto triples = new TripleS();
+
+  TripleS triples = new TripleS();
+
+	version(Windows){
+		import core.stdc.signal;
+		// WORKAROUND: we don't care about viral @nogc attribute here!
+		import std.traits;
+		signal(SIGABRT, cast(ParameterTypeTuple!signal[1])&onSignal);
+		signal(SIGTERM, cast(ParameterTypeTuple!signal[1])&onSignal);
+		signal(SIGINT, cast(ParameterTypeTuple!signal[1])&onSignal);
+	}
+}
+
+private extern(C) void onSignal(int signal)
+nothrow {
+	logInfo("Received signal %d. I am shutting down.", signal);
 }
