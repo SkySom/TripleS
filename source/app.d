fd718dd3d25bce0e930c009389bfa9128e74ea7b
SkySom <skylar.som@gmail.com>
2014-07-15 12:13:40 -0400

    Added Vibe.d
    Note:The bot still lives after server shut down.

--- source/app.d
+++ source/app.d
@@ -1,7 +1,16 @@
 import std.stdio;
+
+import vibe.d;
+
 import triples.bot;
 
-void main()
+shared static this()
 {
+	auto routes = new URLRouter;
+	auto settings = new HTTPServerSettings();
+	settings.port = 8080;
+	settings.bindAddresses = ["::1", "127.0.0.1"];
+	listenHTTP(settings, routes);
+	
 	auto triples = new TripleS();
 }
