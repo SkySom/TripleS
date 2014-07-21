73b546f50b5b751760e3d81e25052d03cba91813
SkySom <skylar.som@gmail.com>
2014-07-21 19:20:15 -0400

    Remove Vibe.d

--- source/app.d
+++ source/app.d
@@ -1,36 +1,7 @@
 import std.stdio;
-import core.atomic;
-import core.sync.condition;
-import core.sync.mutex;
-import core.stdc.stdlib;
-import core.thread;
-
-import vibe.d;
 
 import triples.bot;
 
-shared static this()
-{
-	auto routes = new URLRouter;
-	auto settings = new HTTPServerSettings();
-	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, routes);
-
-  TripleS triples = new TripleS();
-
-	version(Windows){
-		import core.stdc.signal;
-		// WORKAROUND: we don't care about viral @nogc attribute here!
-		import std.traits;
-		signal(SIGABRT, cast(ParameterTypeTuple!signal[1])&onSignal);
-		signal(SIGTERM, cast(ParameterTypeTuple!signal[1])&onSignal);
-		signal(SIGINT, cast(ParameterTypeTuple!signal[1])&onSignal);
-	}
-}
-
-private extern(C) void onSignal(int signal)
-nothrow {
-	logInfo("Received signal %d. I am shutting down.", signal);
-	exit(0);
+void main() {
+	auto triples = new TripleS();
 }
